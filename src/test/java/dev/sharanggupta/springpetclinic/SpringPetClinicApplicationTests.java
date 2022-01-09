c2ec870501be23985436268fabe22f1b3252c920
“sharang1996” <sharang.gupta@sitpune.edu.in>
2022-01-09 23:11:17 +0000

    Initial commit

---
+++ src/test/java/dev/sharanggupta/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
